80379e5559b2590f4f99743fb1c357b4aaf78a25
Travis CI <travis@travis-ci.org>
2018-06-28 19:14:43 +0000

    Deploy code docs to GitHub Pages Travis build: 14
    Commit: bb3fe5a8b10c17af1b514ca7620b1cf625aca60d

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getbuffer',['getBuffer',['../class_t_f_t__framebuffer.html#a11b4785d18cd92d16822454fb4607b95',1,'TFT_framebuffer']]]
+  ['getbuffer',['getBuffer',['../class_t_f_t__framebuffer.html#a11b4785d18cd92d16822454fb4607b95',1,'TFT_framebuffer']]],
+  ['getdirtyrect',['getDirtyRect',['../class_t_f_t__framebuffer.html#ae754bda0546d45f1647ff442d82e2c00',1,'TFT_framebuffer']]]
 ];
